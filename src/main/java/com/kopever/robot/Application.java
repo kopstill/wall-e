a01e6fb46f48b00061e18d37d2671a541d1c02ce
kopmoe <kop.still@gmail.com>
2017-12-29 01:45:48 +0800

    structure

---
+++ src/main/java/com/kopever/robot/Application.java
@@ -0,0 +1,9 @@
+package com.kopever.robot;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
---
+++ src/test/java/com/kopever/robot/ApplicationTest.java
@@ -0,0 +1,9 @@
+package com.kopever.robot;
+
+public class ApplicationTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
