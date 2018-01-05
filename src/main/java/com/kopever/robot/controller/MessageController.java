74960accffca53805e8e4b398906011e8e39824f
kopmoe <kop.still@gmail.com>
2018-01-06 03:55:38 +0800

    fix

--- src/main/java/com/kopever/robot/controller/MessageController.java
+++ src/main/java/com/kopever/robot/controller/MessageController.java
@@ -1,10 +1,12 @@
 package com.kopever.robot.controller;
 
+import com.google.common.collect.Maps;
 import com.kopever.robot.domain.vo.WechatVerificationVO;
 import com.kopever.robot.util.Jackson;
 import com.kopever.robot.util.SignUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +21,17 @@ public class MessageController {
 
     private static Logger logger = LoggerFactory.getLogger(MessageController.class);
 
+    @Value("${business.wechat.token}")
+    private String token;
+
     @GetMapping("/")
     public String signature(WechatVerificationVO requestVO) {
-        Map map = Jackson.fromObject(requestVO, Map.class);
-        logger.info("MessageController.signature.map -> {}", map);
+        logger.info("MessageController.signature.requestVO -> {}", Jackson.toJson(requestVO));
+        Map<Object, Object> map = Maps.newHashMap();
+        map.put("timestamp", requestVO.getTimestamp());
+        map.put("nonce", requestVO.getNonce());
+        map.put("token", token);
+        logger.info("MessageController.signature.map -> {}", Jackson.toJson(map));
 
         String sign = SignUtil.wechatSignWithSHA1(map);
         logger.info("MessageController.signature.sign -> {}", sign);
